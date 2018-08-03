daeb1244233282c16fe64e6d4d2716dc52bd1600
vinthen <nehtniv@gmail.com>
2018-08-03 10:33:00 +0800

    add summercamp file

---
+++ summercamp/js/layout.js
@@ -0,0 +1,51 @@
+//swiper image gallery
+let gallery = new Swiper(".swiper-container", {
+
+  loop: true,
+  autoHeight: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  }
+
+});
+
+//APP download button for mobile
+let trigger = document.getElementById("hamburger");
+let menu = document.querySelector(".download");
+let overlay = document.getElementById("downloadOverlay");
+
+trigger.addEventListener("click",toggleDownloadMenu,false);
+
+overlay.addEventListener("click",hideDownloadMenu,false);
+
+document.querySelectorAll(".download--btn").forEach(function(element){
+  element.addEventListener("click",hideDownloadMenu,false);
+});
+
+//toggle download menu
+function toggleDownloadMenu(){
+
+  if(menu.classList.contains("open")){
+    menu.classList.remove("open");
+    trigger.classList.remove("active");
+    overlay.classList.remove("active");
+  } else {
+    menu.classList.add("open");
+    trigger.classList.add("active");
+    overlay.classList.add("active");
+  }
+}
+
+//hide download menu
+function hideDownloadMenu(){
+  trigger.classList.remove("active");
+  menu.classList.remove("open");
+  overlay.classList.remove("active");
+}
+
